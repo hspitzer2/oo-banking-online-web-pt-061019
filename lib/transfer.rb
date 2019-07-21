9d901051b2fc0db77701301b583e4701c66233f7
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-21 18:58:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
 class Transfer
-  # your code here
+ require 'pry'
+  attr_reader
+  attr_accessor :status, :sender, :receiver, :amount, :transfer
+
+  def initialize(sender, receiver, amount)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+
+  def valid?
+     @sender.valid? && @receiver.valid? ? true : false
+   end
+
+
+
+  def execute_transaction
+    if self.valid? && self.status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+    self.status = "complete"
+    end
+
+  end
+
 end
+  # def reverse_transfer
+  # end
